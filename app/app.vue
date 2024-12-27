b33ec97710cbc3824cde878720e9302bf673ae75
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-12-27 22:00:14 +0700

    first commit

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>111222</h1>
+        <!-- <NuxtRouteAnnouncer /> -->
+        <!-- <NuxtWelcome /> -->
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    future: {
+        compatibilityVersion: 4,
+    },
+    devtools: { enabled: true },
+    routeRules: { "/": { prerender: true } },
+    css: ["./app/assets/scss/main.scss"],
+    compatibilityDate: "2024-12-27",
+});
