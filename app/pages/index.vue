01aacc6a3b5b0c21a4f428768da3ae2f2033ebac
Konstantin <163849606+it2konst@users.noreply.github.com>
2025-01-06 09:41:39 +0700

    feat: section (ProCard)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
 import MainHero from "@/components/main/MainHero.vue";
+
+useSeoMeta({
+    title: "Portfolio Hub | Modern Web Development Projects",
+    description:
+        "Explore my portfolio showcasing modern web development projects built with Vue 3, Nuxt 3, TypeScript, and Tailwind CSS. Each project includes live demos and GitHub links.",
+    // Open Graph (для Facebook, LinkedIn и других соцсетей)
+    ogTitle: "Portfolio Hub | Modern Web Development Projects",
+    ogDescription:
+        "Explore my portfolio showcasing modern web development projects built with Vue 3, Nuxt 3, TypeScript, and Tailwind CSS.",
+    ogImage: "/images/opengraph.jpg",
+    ogUrl: "https://konst-portfolio.vercel.app",
+    // Twitter Card
+    twitterTitle: "Portfolio Hub | Modern Web Development Projects",
+    twitterDescription:
+        "Explore my portfolio showcasing modern web development projects built with Vue 3, Nuxt 3, TypeScript, and Tailwind CSS.",
+    twitterImage: "/images/opengraph.jpg",
+    twitterCard: "summary",
+});
 </script>
 
 <template>
