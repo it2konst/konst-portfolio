7a044930b0998df07e826073b9fdda125228cff6
Konstantin <163849606+it2konst@users.noreply.github.com>
2025-01-06 10:43:28 +0700

    feat: ReadMe

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,17 +2,17 @@
 import MainHero from "@/components/main/MainHero.vue";
 
 useSeoMeta({
-    title: "Portfolio Hub | Modern Web Development Projects",
+    title: "Konst Portfolio | Modern Web Development Projects",
     description:
         "Explore my portfolio showcasing modern web development projects built with Vue 3, Nuxt 3, TypeScript, and Tailwind CSS. Each project includes live demos and GitHub links.",
     // Open Graph (для Facebook, LinkedIn и других соцсетей)
-    ogTitle: "Portfolio Hub | Modern Web Development Projects",
+    ogTitle: "Konst Portfolio | Modern Web Development Projects",
     ogDescription:
         "Explore my portfolio showcasing modern web development projects built with Vue 3, Nuxt 3, TypeScript, and Tailwind CSS.",
     ogImage: "https://konst-portfolio.vercel.app/images/opengraph.jpg",
     ogUrl: "https://konst-portfolio.vercel.app",
     // Twitter Card
-    twitterTitle: "Portfolio Hub | Modern Web Development Projects",
+    twitterTitle: "Konst Portfolio | Modern Web Development Projects",
     twitterDescription:
         "Explore my portfolio showcasing modern web development projects built with Vue 3, Nuxt 3, TypeScript, and Tailwind CSS.",
     twitterImage: "https://konst-portfolio.vercel.app/images/opengraph.jpg",
