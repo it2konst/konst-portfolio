dc5230ba6b321b2193e4fe01f2acaeabd5e1d1dc
Konstantin <163849606+it2konst@users.noreply.github.com>
2025-01-08 23:47:45 +0700

    feat: section (DarkMode)

--- app/components/block/DarkMode.vue
+++ app/components/block/DarkMode.vue
@@ -1,23 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-    <input type="checkbox" id="themeSwitch" aria-label="Переключить на дневную / ночную тему" v-dark />
+    <div class="switch">
+        <input type="checkbox" id="themeSwitch" aria-label="Переключить на дневную / ночную тему" v-dark />
 
-    <label for="themeSwitch">
-        <div class="switch-block">
-            <div class="indicator">
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
-                    <path
-                        d="M10.6067 2.12132C9.83451 1.34916 8.89689 0.8358 7.9126 0.572756C8.44717 2.57528 7.93381 4.79418 6.36403 6.36396C4.79425 7.93374 2.57535 8.4471 0.572826 7.91253C0.83587 8.89682 1.34923 9.83444 2.12139 10.6066C4.46333 12.9485 8.26473 12.9485 10.6067 10.6066C12.9486 8.26466 12.9486 4.46326 10.6067 2.12132Z"
-                        fill="currentColor"
-                    />
-                </svg>
+        <label for="themeSwitch">
+            <div class="switch-block">
+                <div class="indicator">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
+                        <path
+                            d="M10.6067 2.12132C9.83451 1.34916 8.89689 0.8358 7.9126 0.572756C8.44717 2.57528 7.93381 4.79418 6.36403 6.36396C4.79425 7.93374 2.57535 8.4471 0.572826 7.91253C0.83587 8.89682 1.34923 9.83444 2.12139 10.6066C4.46333 12.9485 8.26473 12.9485 10.6067 10.6066C12.9486 8.26466 12.9486 4.46326 10.6067 2.12132Z"
+                            fill="currentColor"
+                        />
+                    </svg>
+                </div>
             </div>
-        </div>
-    </label>
+        </label>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.switch {
+    position: relative;
+    padding: 0 1rem;
+}
+
 input {
     position: absolute;
     visibility: hidden;
