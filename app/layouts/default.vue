950a4b958bed509a87d363c81f514c645dfba275
Konstantin <163849606+it2konst@users.noreply.github.com>
2025-01-10 08:52:22 +0700

    feat: Animation BG

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppBgAnimation from "~/components/AppBgAnimation.vue";
+</script>
 
 <template>
-    <AppBg />
+    <!-- <AppBg /> -->
+    <AppBgAnimation />
     <AppHeader />
     <main>
         <slot />
